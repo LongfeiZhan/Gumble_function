3a88adafccdfc1d0d587aeae995a144fa402baf0
LF Zhan <lf.zhan@foxmail.com>
2021-07-16 09:39:23 +0800

    加入缺测值剔除功能

--- Gumble_func.m
+++ Gumble_func.m
@@ -1,22 +1,22 @@
 function [Recurrence_period_value] = Gumble_func(data,N,len)
-% dataÓ¦ÎªÒ»ÁĞÊı¾İ
-% NÎª¶àÉÙÄêÒ»Óö
-% lenÎªËùÓÃÊı¾İÄêÊı
-% Recurrence_period_valueÎªNÄêÒ»ÓöÖµ
+% dataåº”ä¸ºä¸€åˆ—æ•°æ®
+% Nä¸ºå¤šå°‘å¹´ä¸€é‡
+% lenä¸ºæ‰€ç”¨æ•°æ®å¹´æ•°
+% Recurrence_period_valueä¸ºNå¹´ä¸€é‡å€¼
 datasort = sort(data,'descend');
 data_select_sort = datasort(1:len);
-
+data_select_sort(isnan(data_select_sort))=[];%å‰”é™¤ç¼ºæµ‹å€¼
 mu = mean(data_select_sort);
 sum = 0;
 for i = 1:length(data_select_sort)
     sum = sum + (data_select_sort(i) - mu)^2;
 end
-sigma = sqrt(sum/(length(data_select_sort)-1)); %±ê×¼²î
+sigma = sqrt(sum/(length(data_select_sort)-1)); %æ ‡å‡†å·®
 alpha = pi/(sqrt(6)*sigma);
 u = mu - 0.57721/alpha;
-for i = 1 : 0.1 : 500 %½µË®Á¿ÉèÖÃÎª1~500mm
-    if 1/N >= 1 - exp(-exp(-alpha*(i - u)))  % NÄêÒ»Óö
-        Recurrence_period_value = i;  %  Recurrence_period_value¼´ÎªNÄêÒ»Óö½µË®Öµ
+for i = 1 : 0.1 : 500 %é™æ°´é‡è®¾ç½®ä¸º1~500mm
+    if 1/N >= 1 - exp(-exp(-alpha*(i - u)))  % Nå¹´ä¸€é‡
+        Recurrence_period_value = i;  %  Recurrence_period_valueå³ä¸ºNå¹´ä¸€é‡é™æ°´å€¼
         break;
     end
 end
